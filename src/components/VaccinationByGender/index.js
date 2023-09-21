a46c2e8b219b74f73df13f71966e4d01aeb79ade
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-21 17:07:23 +0530

    Update index.js

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,9 +1,9 @@
 // Write your code here
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
 import './index.css'
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
 
-const VaccinebyGender = props => {
-  const {GenderVaccine} = props
+const VaccinebyAge = props => {
+  const {AgeVaccine} = props
 
   return (
     <ResponsiveContainer width="100%" height={300}>
@@ -11,16 +11,17 @@ const VaccinebyGender = props => {
         <Pie
           cx="70%"
           cy="40%"
-          data={GenderVaccine}
+          data={AgeVaccine}
           startAngle={0}
-          endAngle={180}
+          endAngle={360}
           innerRadius="40%"
           outerRadius="70%"
           dataKey="count"
-        />
-        <Cell name="Male" fill="#f54394" />
-        <Cell name="Female" fill="#5a8dee" />
-        <Cell name="Others" fill="#2cc6c6" />
+        >
+          <Cell name="Male" fill="#f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill="#2cc6c6" />
+        </Pie>
         <Legend
           iconType="circle"
           layout="vertical"
@@ -31,4 +32,5 @@ const VaccinebyGender = props => {
     </ResponsiveContainer>
   )
 }
-export default VaccinebyGender
+
+export default VaccinebyAge
