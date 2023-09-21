1fcda6a65bc434c85158a3545db8c743b1a0db7b
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-21 17:06:56 +0530

    Update index.js

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -13,14 +13,14 @@ const VaccinebyGender = props => {
           cy="40%"
           data={GenderVaccine}
           startAngle={0}
-          endAngle={360}
+          endAngle={180}
           innerRadius="40%"
           outerRadius="70%"
           dataKey="count"
         />
-        <Cell name="male" fill="# #f54394" />
-        <Cell name="female" fill="#5a8dee" />
-        <Cell name="others" fill="#2cc6c6" />
+        <Cell name="Male" fill="#f54394" />
+        <Cell name="Female" fill="#5a8dee" />
+        <Cell name="Others" fill="#2cc6c6" />
         <Legend
           iconType="circle"
           layout="vertical"
