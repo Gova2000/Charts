0e3b33f4b482d7f6d4e1443324a673af5e159884
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-28 11:25:37 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -21,33 +21,36 @@ const VaccinebyCoverage = props => {
   }
 
   return (
-    <ResponsiveContainer width="100%" height={500}>
-      <BarChart data={vaccineList}>
-        <XAxis
-          dataKey="vaccineDate"
-          tick={{
-            stroke: 'gray',
-            strokeWidth: 1,
-          }}
-        />
+    <>
+      <h1>Vaccination Coverage</h1>
+      <ResponsiveContainer width={1000} height={500}>
+        <BarChart data={vaccineList}>
+          <XAxis
+            dataKey="vaccineDate"
+            tick={{
+              stroke: 'gray',
+              strokeWidth: 1,
+            }}
+          />
 
-        <YAxis
-          tickFormatter={DataFormatter}
-          tick={{
-            stroke: 'gray',
-            strokeWidth: 0,
-          }}
-        />
+          <YAxis
+            tickFormatter={DataFormatter}
+            tick={{
+              stroke: 'gray',
+              strokeWidth: 0,
+            }}
+          />
 
-        <Legend
-          wrapperStyle={{
-            padding: 30,
-          }}
-        />
-        <Bar dataKey="dose1" name="Dose1" fill="#5a8dee" barSize="20%" />
-        <Bar dataKey="dose2" name="Dose2" fill="#f54394" barSize="20%" />
-      </BarChart>
-    </ResponsiveContainer>
+          <Legend
+            wrapperStyle={{
+              padding: 30,
+            }}
+          />
+          <Bar dataKey="dose1" name="Dose1" fill="#5a8dee" barSize="20%" />
+          <Bar dataKey="dose2" name="Dose2" fill="#f54394" barSize="20%" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   )
 }
 
