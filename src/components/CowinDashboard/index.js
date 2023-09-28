fb05850e17681a42d5249e5af0339b93204c54d1
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-28 11:23:23 +0530

    Update index.js

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -1,19 +1,32 @@
 // Write your code here
 import {Component} from 'react'
-
+import Loader from 'react-loader-spinner'
 import VaccinebyCoverage from '../VaccinationCoverage'
-import VaccinebyAge from '../VaccinationByGender'
-import VaccinebyGender from '../VaccinationByAge'
+import VaccinebyGender from '../VaccinationByGender'
+import VaccinebyAge from '../VaccinationByAge'
 import './index.css'
 
+const content = {
+  success: 'success',
+  failure: 'failure',
+  progress: 'progress',
+}
+
 class CowinDashboard extends Component {
-  state = {vaccineList: [], GenderVaccine: [], AgeVaccine: []}
+  state = {
+    vaccineList: [],
+    GenderVaccine: [],
+    AgeVaccine: [],
+
+    key: '',
+  }
 
   componentDidMount() {
     this.GetFetch()
   }
 
   GetFetch = async () => {
+    this.setState({key: content.progress})
     const get1 = await fetch('https://apis.ccbp.in/covid-vaccination-data')
 
     if (get1.ok === true) {
@@ -35,35 +48,85 @@ class CowinDashboard extends Component {
         vaccineList: update,
         GenderVaccine: format.getbyGender,
         AgeVaccine: format.getbyAge,
+
+        key: content.success,
       })
+    } else {
+      this.setState({key: content.failure})
     }
   }
 
   get = () => {
     const {vaccineList, GenderVaccine, AgeVaccine} = this.state
-    console.log(GenderVaccine)
+    console.log(GenderVaccine, AgeVaccine)
     return (
-      <div>
-        <div>
+      <div className="mbg1">
+        <div className="row">
           <img
             src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
             alt="website logo"
           />
-          <h1>Co-Win</h1>
+          <h1 className="h1">Co-Win</h1>
         </div>
         <h1>Cowin Vaccination in India</h1>
 
         <VaccinebyCoverage vaccineList={vaccineList} />
-
         <VaccinebyGender GenderVaccine={GenderVaccine} />
-
         <VaccinebyAge AgeVaccine={AgeVaccine} />
       </div>
     )
   }
 
+  progress = () => (
+    <div className="mbg1">
+      <div className="row">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+          alt="website logo"
+        />
+        <h1 className="h1">Co-Win</h1>
+      </div>
+      <h1>Cowin Vaccination in India</h1>
+      <div className="main" data-testid="loader">
+        <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+      </div>
+    </div>
+  )
+
+  failureCase = () => (
+    <div className="mbg1">
+      <div className="row">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+          alt="website logo"
+        />
+        <h1 className="h1">Co-Win</h1>
+      </div>
+      <h1>Cowin Vaccination in India</h1>
+      <div className="mbg1">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png "
+          alt="failure view"
+        />
+        <h1> Something went wrong</h1>
+      </div>
+    </div>
+  )
+
   render() {
-    return <div>{this.get()}</div>
+    const {key} = this.state
+
+    switch (key) {
+      case content.success:
+        return this.get()
+      case content.progress:
+        return this.progress()
+      case content.failure:
+        return this.failureCase()
+
+      default:
+        return null
+    }
   }
 }
 
